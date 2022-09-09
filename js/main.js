654e331b83e6cdb4707ce68d347ad610ad722e74
DiasSarkytbayev1 <diassarkytbayev@gmail.com>
2022-09-09 15:48:20 +0600

    later do

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const birthday = window.prompt('Enter your birthday (DD.MM.YYYY)');
+
+
+
+document.querySelector('.countdown').innerHTML = birthday;
+document.querySelector('.countdown').innerHTML = new Date();
+
+
+
